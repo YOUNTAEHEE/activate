e4ab5eb4e652260027c6e78617fc44bbb4af9250
YOUNTAEHEE <f_hyang@naver.com>
2023-11-13 11:34:59 +0900

    활성화된 버튼 또 클릭안되게

--- js/main.js
+++ js/main.js
@@ -4,11 +4,16 @@ const boxs = main.querySelectorAll('article');
 
 btns.forEach((btn, idx) => {
 	btn.addEventListener('click', () => {
-		//직전의 모든 요소 초기화
-		btns.forEach((el) => el.classList.remove('on'));
-		boxs.forEach((el) => el.classList.remove('on'));
-		//클릭한 순번에 해당하는 버튼만 활성화
-		btns[idx].classList.add('on');
-		boxs[idx].classList.add('on');
+		//만약 클릭한 버튼에 클래스 .on이 있다면 리턴으로 함수 실행 중지.
+		//만약 클릭한 버튼에 on이 없다면 해당 조건문이 무시되면서 activation함수 호출
+		if (btns[idx].classList.contains('on')) return;
+		[btns, boxs].forEach((el) => {
+			activation(el, idx);
+		});
 	});
 });
+function activation(arr, index) {
+	console.log('activation');
+	arr.forEach((el) => el.classList.remove('on'));
+	arr[index].classList.add('on');
+}
