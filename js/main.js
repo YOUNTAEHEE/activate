9b7ea0393c9952abe2f0007270570adaf3a8a0cc
YOUNTAEHEE <f_hyang@naver.com>
2023-11-13 12:11:21 +0900

    innerHTML

--- js/main.js
+++ js/main.js
@@ -12,3 +12,20 @@ btns.forEach((btn, idx) => {
 		boxs[idx].classList.add('on');
 	});
 });
+console.dir(btns[0]);
+//DOM안에 자주 쓰이는 유용한 property
+//DOM.innerText:해당 돔에 연결되어 있는 text값을 확인가능
+//DOM.innerHTML:해당 돔에 연결되어 있는 자식 태그 구조를 확인가능
+//DOM.className:해당 돔에 연결되어 있는 모든 클래스명을 문자열로 확인가능
+
+//미션 내가 버튼클릭하며 클릭한 버튼의 문자값.
+btns.forEach((btn) => {
+	btn.addEventListener('click', (e) => {
+		//클릭시 해당 li의 글자 확인
+		console.log(e.currentTarget.innerText);
+		//클릭한 li의 글자값을 순번으로 변경(innerText객체의 key이기 때문에 대입연산자로 값 변경 가능)
+		e.currentTarget.innerText = idx;
+		//클릭한 li의 글자값을 가져와서 <span>으로 감싼 다음에 li에 innerHTML로 등록
+		e.currentTarget.innerText = `<span>${e.currentTarget.innerText}</span>`;
+	});
+});
